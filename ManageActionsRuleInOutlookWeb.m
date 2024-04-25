3c23ab9746ac62f359571e17c5de195f163e1102
Kimya B <kimyabuckner@gmail.com>
2024-04-25 15:32:49 -0400

    Update and rename HowTo/CreateCustomActionsRuleInOutlookWeb.md to ManageActionsRuleInOutlookWeb.m

---
+++ ManageActionsRuleInOutlookWeb.m
@@ -0,0 +1,14 @@
+# Manage custom action rules using Outlook web
+
+A rule is an automated action that Outlook performs on incoming or outgoing emails. Rules allow you to organize, filter, and process messages automatically based on specified criteria. You can create rules to move incoming emails to specific folders, delete emails that meet certain conditions, forward emails to another address, and more.
+
+## Create a rule
+
+1. 
+
+
+> **Note:** Rules will only automatically operate on messages as they are received. To run a rule on messages that are already in the **Inbox**, navigate to the rule by following steps one and two above and select **Run rule now**. 
+
+## Delete a rule
+
+
